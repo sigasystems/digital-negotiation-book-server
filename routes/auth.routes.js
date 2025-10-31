b0a5fb8002ac051b7d8ddfc01878daf02630b7fe
sigaDurgesh <durgeshs@sigasystems.com>
2025-10-31 10:54:16 +0530

    rate limiter removed

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -8,8 +8,8 @@ import { authenticateRefreshToken } from "../middlewares/authenticateRefreshToke
 const router = express.Router()
 
 // router.post('/register', rateLimiter, register)
-router.post('/login', rateLimiter, authcontroller.login)
-router.post('/refresh-token', rateLimiter, authenticateRefreshToken, authcontroller.refreshTokenRotation)
+router.post('/login', authcontroller.login)
+router.post('/refresh-token', authenticateRefreshToken, authcontroller.refreshTokenRotation)
 // router.post('/reset-password', authenticateJWT, authController.resetPassword)
 router.post("/request-reset", requestPasswordReset);
 router.post("/reset-password-otp", resetPasswordWithOtp);
