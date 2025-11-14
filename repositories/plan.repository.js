a2ce6c3a4b39bccfa57122ebd389b8027cb95eba
SigaLalit <lalitk@sigasystems.com>
2025-11-14 16:26:13 +0530

    Enhance subscription handling: import Stripe, update upsertSubscription method, and improve payment processing logic

--- repositories/plan.repository.js
+++ repositories/plan.repository.js
@@ -1,4 +1,7 @@
+import Stripe from "stripe";
 import { Payment, Plan, Subscription, User } from "../models/index.js";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export const PlanRepository = {
   create: async (data) => {
@@ -40,24 +43,48 @@ createPayment : async (paymentData) => {
  getPaymentBySession : async (sessionId) => {
   return await Payment.findOne({ where: { sessionId } });
 },
- upsertSubscription: async (data)=> {
-    // Create or update user’s plan record
-    const { userId, planId,  status, startDate, endDate } = data;
+//  upsertSubscription: async (data)=> {
+//     // Create or update user’s plan record
+//     const { userId, planId,  status, startDate, endDate } = data;
 
-    const existing = await User.findOne({ where: { id } });
-    if (existing) {
-      await existing.update({ planId,  status, startDate, endDate });
-    } else {
-      await User.create({ userId, planId,  status, startDate, endDate });
-    }
-  },
+//     const existing = await User.findOne({ where: { userId } });
+//     console.log('existing user....',existing);
+//     if (existing) {
+//       await existing.update({ planId,  status, startDate, endDate });
+//     } else {
+//       await User.create({ userId, planId,  status, startDate, endDate });
+//     }
+//   },
 
+upsertSubscription: async (data) => {
+  const { id, userId, planId, status, startDate, endDate, subscriptionId } = data;
+
+  return await Subscription.upsert({
+    id: id || undefined,            // allows insert or update
+    userId,
+    planId,
+    status,
+    startDate,
+    endDate,
+    subscriptionId
+  });
+},
    markPaid : async(subscriptionId) => {
     // const plan = await User.findOne({ where: { subscriptionId: subscriptionId } });
     const plan = await Payment.findOne({ subscriptionId  });
 console.log('Plannn......',plan);
     if (plan) {
-      const subscription = await stripe.subscriptions.retrieve(subscriptionId);
+
+     const subscriptionId = event.data.object.subscription;
+
+if (!subscriptionId) {
+  console.log("No subscription ID found in event, skipping");
+  return res.status(200).send("OK");
+}
+
+const subscription = await stripe.subscriptions.retrieve(subscriptionId);
+
+      // const subscription = await stripe.subscriptions.retrieve(subscriptionId);
       await Payment.update({
         status: subscription.status,
         endDate: new Date(subscription.current_period_end * 1000),
